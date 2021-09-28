afabda3c400cb5263a648c7b500bf83e63ad74db
k-chiranjeevi <chiru8004@gmail.com>
2021-09-28 20:30:50 +0530

    portfolio added

---
+++ index.js
@@ -0,0 +1,9 @@
+var nav_links = document.getElementById('nav_links');
+
+function showMenu() {
+    nav_links.style.right = "0px";
+}
+function hideMenu() {
+    nav_links.style.right = "-200px";
+}
+
